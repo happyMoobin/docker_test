d97b3e62e2edb9ab40c01b34f971f4b145464cad
happyMoobin <jnjhomf@naver.com>
2024-12-16 17:13:40 +0900

    first

---
+++ deploy-dev.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+docker rm -fv webserver dbserver
+docker network ls | grep myapp-net
+if [ $? -ne 0 ]
+	then
+		docker network crate myapp-net --subnet 172.20.0.0/24
+fi
+
+docker run \
+  -d \
+  -p 3306:3306 \
+  -e MYSQL_ROOT_PASSWORD=Test123! \
+  -e MYSQL_DATABASE=webtest \
+  -e MYSQL_USER=testuser \
+  -e MYSQL_PASSWORD=testpass \
+  -v mydata:/var/lib/mysql \
+  -v ./create_db_webtest.sql:/docker-entrypoint-initdb.d/webtest.sql \
+  --net myapp-net \
+  --name dbserver \
+  quay.io/uvelyster/mysql:5.7
+
+docker run -d -p 2222:80 -v /source:/var/www/html --net myapp-net --name webserver php:7.3-apache
+docker exec webserver docker-php-ext-install mysqli
+docker restart webserver
+
+
